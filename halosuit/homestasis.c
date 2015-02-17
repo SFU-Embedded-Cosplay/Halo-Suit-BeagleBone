f8216c7035e95dc687055fb69589d7b85e315100
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-02-17 15:39:22 -0800

    Refactored the homeostasis implementation Fixed spacing on the serializer

--- halosuit/homestasis.c
+++ halosuit/homestasis.c
@@ -6,8 +6,70 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 #include <homestasis.h>
 #include <halosuit.h>
+
+void checkHeadTemp(int temp)
+{
+    if (temp >= HIGH_TEMP) {
+        if (halosuit_relay_switch(HEAD_FANS, HIGH)) {
+            printf("ERROR: HEAD_FANS READ FAILURE");
+        }
+
+        if (temp >= MAX_TEMP) {
+            // send warning
+        }
+        else {
+            // send critical warning
+        }
+    }
+    else if (temp <= LOW_TEMP) {
+        if (halosuit_relay_switch(HEAD_FANS, LOW)) {
+            printf("ERROR: HEAD_FANS READ FAILURE");
+        }
+
+        if (temp <= MINIMUM_TEMP) {
+            // send critical warning
+        }
+        else {
+            // send warning
+        }
+    }
+}
+
+void checkBodyTemp(int temp)
+{
+    if (tempemp >= HIGH_TEMP) {
+        if (halosuit_relay_switch(WATER_PUMP, HIGH)) {
+            printf("ERROR: WATER_FANS READ FAILURE");
+        }
+        if (halosuit_relay_switch(WATER_FAN, HIGH)) {
+            printf("ERROR: HEAD_FANS READ FAILURE");
+        }
+        if (temp >= MAX_TEMP) {
+            // send critical warning
+        }
+        else {
+            // send warning
+        }
+    }
+    else if (temp <= LOW_TEMP) {
+        if (halosuit_relay_switch(WATER_PUMP, LOW)) {
+            printf("ERROR: WATER_FANS READ FAILURE");
+        }
+        if (halosuit_relay_switch(WATER_FAN, LOW)) {
+            printf("ERROR: HEAD_FANS READ FAILURE");
+        }
+
+        if (temp <= MINIMUM_TEMP) {
+            // send critical warning
+        }
+        else {
+            // send warning
+        }
+    }
+}
 
 void* homeostasis_thread()
 { 
@@ -16,109 +78,15 @@ void* homeostasis_thread()
     double crotchTemp = 0;
 
     while (1) {
-	halosuit_temperature_value(HEAD, &headTemp);
-	halosuit_temperature_value(ARMPITS, &armpitsTemp);
-	halosuit_temperature_value(CROTCH, &crotchTemp);
-	halo
+        halosuit_temperature_value(HEAD, &headTemp);
+        halosuit_temperature_value(ARMPITS, &armpitsTemp);
+        halosuit_temperature_value(CROTCH, &crotchTemp);
 
-	if (headTemp >= MAX_TEMP) {
-	    // critical alert
-	    if (halosuit_relay_switch(HEAD_FANS, HIGH)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (headTemp >= HIGH_TEMP) {
-	    // sends warning
-	    if (halosuit_relay_switch(HEAD_FANS, HIGH)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (headTemp <= LOW_TEMP) {
-	    // sends warning 
-	    if (halosuit_relay_switch(HEAD_FANS, LOW)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (headTemp <= MINIMUM_TEMP) {
-	    // critical alert
-	    if (halosuit_relay_switch(HEAD_FANS, LOW)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	
-	if (armpitsTemp >= MAX_TEMP) {
-	    // critical alert 
-	    if (halosuit_relay_switch(WATER_PUMP, HIGH)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, HIGH)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (armpitsTemp >= HIGH_TEMP) {
-	    // sends warning  
-	    if (halosuit_relay_switch(WATER_PUMP, HIGH)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, HIGH)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (armpitsTemp <= LOW_TEMP) {
-	    // sends warning
-	    if (halosuit_relay_switch(WATER_PUMP, LOW)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, LOW)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (armpitsTemp <= MINIMUM_TEMP) {
-	    // critical alert 
-	    if (halosuit_relay_switch(WATER_PUMP, LOW)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, LOW)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-
-	if (crotchTemp >= MAX_TEMP) {
-	    // critical alert 
-	    if (halosuit_relay_switch(WATER_PUMP, HIGH)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, HIGH)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (crotchTemp >= HIGH_TEMP) {
-	    // sends warning
-	    if (halosuit_relay_switch(WATER_PUMP, HIGH)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, HIGH)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (crotchTemp <= LOW_TEMP) {
-	    // sends warning 
-	    if (halosuit_relay_switch(WATER_PUMP, LOW)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, LOW)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	}
-	else if (crotchTemp <= MINIMUM_TEMP) {
-	    // critical alert
-	    if (halosuit_relay_switch(WATER_PUMP, LOW)) {
-		printf("ERROR: WATER_FANS READ FAILURE");
-	    }
-	    if (halosuit_relay_switch(WATER_FAN, LOW)) {
-		printf("ERROR: HEAD_FANS READ FAILURE");
-	    }
-	} 
-	
+        averageBodyTemp = (armpitsTemp + crotchTemp) / 2; 
+        
+        checkHeadTemp(headTemp);
+        checkBodyTemp(averageBodyTemp); 
     }
 }
+
+
--- json/serializer.c
+++ json/serializer.c
@@ -21,87 +21,87 @@ static void serializer_buildjson(json_value *object)
     int value = 0;
     // lights
     if (halosuit_relay_value(LIGHTS, &value) != 0) {
-	printf("ERROR: LIGHTS READ VALUE FAILURE\n");
+	    printf("ERROR: LIGHTS READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "lights", json_string_new(ON));
+	    json_object_push(object, "lights", json_string_new(ON));
     }
     // auto lights
     else if (halosuit_relay_value(LIGHTS_AUTO, &value) != 0) {
-	printf("ERROR: LIGHTS_AUTO READ VALUE FAILURE\n");
+	    printf("ERROR: LIGHTS_AUTO READ VALUE FAILURE\n");
     }	
     else if (value == 1) {
-	json_object_push(object, "lights", json_string_new(AUTO));
+	    json_object_push(object, "lights", json_string_new(AUTO));
     }
     else {
-	json_object_push(object, "lights", json_string_new(OFF));
+	    json_object_push(object, "lights", json_string_new(OFF));
     }
     
     
     // head lights white
     if (halosuit_relay_value(HEADLIGHTS_WHITE, &value) != 0) {
-	printf("ERROR: HEADLIGHTS_WHITE READ VALUE FAILURE\n");
+	    printf("ERROR: HEADLIGHTS_WHITE READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "head lights white", json_string_new(ON));
+	    json_object_push(object, "head lights white", json_string_new(ON));
     }
     else if (value == 0) {
-	json_object_push(object, "head lights white", json_string_new(OFF));
+	    json_object_push(object, "head lights white", json_string_new(OFF));
     }
 
     // head lights red
     if (halosuit_relay_value(HEADLIGHTS_RED, &value) != 0) {
-	printf("ERROR: HEADLIGHTS RED READ VALUE FAILURE\n");
+	    printf("ERROR: HEADLIGHTS RED READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "head lights red", json_string_new(ON));
+	    json_object_push(object, "head lights red", json_string_new(ON));
     }
     else if (value == 0) {
-	json_object_push(object, "head lights red", json_string_new(OFF));
+	    json_object_push(object, "head lights red", json_string_new(OFF));
     }
     
     // head fans
     if (halosuit_relay_value(HEAD_FANS, &value) != 0) {
-	printf("ERROR: HEAD_FANS READ VALUE FAILURE\n");
+	    printf("ERROR: HEAD_FANS READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "head fans", json_string_new(ON));
+	    json_object_push(object, "head fans", json_string_new(ON));
     }
     else if (value == 0) {
-	json_object_push(object, "head fans", json_string_new(OFF));
+	    json_object_push(object, "head fans", json_string_new(OFF));
     }
     
     // water pump
     if (halosuit_relay_value(WATER_PUMP, &value) != 0) {
-	printf("ERROR: WATER_PUMP READ VALUE FAILURE\n");
+	    printf("ERROR: WATER_PUMP READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "water pump", json_string_new(ON));
+	    json_object_push(object, "water pump", json_string_new(ON));
     }
     else if (value == 0) {
-	json_object_push(object, "water pump", json_string_new(OFF));
+	    json_object_push(object, "water pump", json_string_new(OFF));
     }
 
     // water fan
     if (halosuit_relay_value(WATER_FAN, &value) != 0) {
-	printf("ERROR: WATER_FAN READ VALUE FAILURE\n");
+	    printf("ERROR: WATER_FAN READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "water fan", json_string_new(ON));
+	    json_object_push(object, "water fan", json_string_new(ON));
     }
     else if (value == 0) {
-	json_object_push(object, "water fan", json_string_new(OFF));
+	    json_object_push(object, "water fan", json_string_new(OFF));
     }
     
     // peltier
     if (halosuit_relay_value(PELTIER, &value) != 0) {
-	printf("ERROR: PELTIER READ VALUE FAILURE\n");
+	    printf("ERROR: PELTIER READ VALUE FAILURE\n");
     }	
     if (value == 1) {
-	json_object_push(object, "peltier", json_string_new(ON));
+	    json_object_push(object, "peltier", json_string_new(ON));
     }
     else if (value == 0) {
-	json_object_push(object, "peltier", json_string_new(OFF));
+	    json_object_push(object, "peltier", json_string_new(OFF));
     }
 
     // temperature readings 
@@ -109,25 +109,25 @@ static void serializer_buildjson(json_value *object)
     
     // head temperature
     if (halosuit_temperature_value(HEAD, &temperature) != 0) {
-	printf("ERROR: HEAD TEMPERATURE READ FAILURE\n");
+	    printf("ERROR: HEAD TEMPERATURE READ FAILURE\n");
     }
     json_object_push(object, "head temperature", json_double_new(temperature)); 
 
     // armpits temperature 
     if (halosuit_temperature_value(ARMPITS, &temperature) != 0) {
-	printf("ERROR: ARMPITS TEMPERATURE READ FAILURE\n");
+	    printf("ERROR: ARMPITS TEMPERATURE READ FAILURE\n");
     }
     json_object_push(object, "armpits temperature", json_double_new(temperature));
 
     // crotch temperature 
     if (halosuit_temperature_value(CROTCH, &temperature) != 0) {
-	printf("ERROR: CROTCH TEMPERATURE READ FAILURE\n");
+	    printf("ERROR: CROTCH TEMPERATURE READ FAILURE\n");
     }
     json_object_push(object, "crotch temperature", json_double_new(temperature));
 
     // water temperature 
     if (halosuit_temperature_value(WATER, &temperature) != 0) {
-	printf("ERROR: WATER TEMPERATURE READ FAILURE\n");
+	    printf("ERROR: WATER TEMPERATURE READ FAILURE\n");
     }
     json_object_push(object, "water temperature", json_double_new(temperature));
 }
