3ee54f50dba7ad082e12cc81f5225e53f31a69d3
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-03-21 20:36:57 -0700

    stopped using ctime() since it adds a newline character to the time string

--- halosuit/logger.c
+++ halosuit/logger.c
@@ -12,39 +12,47 @@
 
 void logger_log(char* log_post)
 {
-    time_t current_time;
-    char* c_time_string;
+    time_t current_time = time(NULL);
+    char[1000]  time_string;
 
-    current_time = time(NULL);
+    // had to use these functions since ctime() added an annoying '\n' onto the string 
+    struct tm * time = localtime(&current_time);
+    strftime(time_string, 1000, "%A, %B %d %Y", time);
 
-    c_time_string = ctime(&current_time);
 
-    if (current_time == ((time_t) - 1) || c_time_string == NULL) {
+
+    if (current_time == ((time_t) - 1) || time_string == NULL) {
         printf("Unable to compute time");
+        return
     }
     
     FILE* f = fopen(LOG_FILE,"w");
     if (f == NULL) {
         printf("Unable to open log file");
     } else {
-        fprintf(f, "[%s]  -  %s\n", c_time_string, log_post);
+        fprintf(f, "[%s]  -  %s\n", time_string, log_post);
         fclose(f);
     }
 }
 
 void logger_startup() 
 {
-    time_t current_time;
-    char* c_time_string;
+    time_t current_time = time(NULL);
+    char[1000]  time_string;
 
-    current_time = time(NULL);
-    c_time_string = ctime(&current_time);
+    struct tm * time = localtime(&current_time);
+    strftime(time_string, 1000, "%A, %B %d %Y", time);
+
+    if (current_time == ((time_t) - 1) || time_string == NULL) {
+        printf("Unable to compute time");
+        return
+    }
 
     FILE* f = fopen(LOG_FILE, "w");
     if (f == NULL) {
         printf("Unable to open log file");
     } else {
-        fprintf(f, "[%s] - %s\n", c_time_string, "start up info");
+        fprintf(f, "[%s] - %s\n", time_string, "start up info");
         fclose(f);
     }
 }
