c5739a45d95714f4dae0ad86b124d88c18d698c8
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-04-12 16:13:01 -0700

    add bluez installation

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "Installing bluetooth"
 apt-get install libbluetooth-dev
+apt-get install bluez
 
 echo "Install python dependencies"
 apt-get update && apt-get install python-serial python-setuptools python-dev python-smbus python-pip
