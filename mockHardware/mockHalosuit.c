b953db0c5b51033ff03a55551974e09e9101a84a
Tyler-R <Tyler539539@hotmail.com>
2015-08-21 17:14:28 -0700

    Add functionality to the mock bluetooth module including a enum that represents the hardware components and filling in some of the functions.

--- mockHardware/mockHalosuit.c
+++ mockHardware/mockHalosuit.c
@@ -9,13 +9,61 @@
 #include <halosuit/halosuit.h>
 
 typedef union {
-int intVal;
-double dblVal;
+	int intVal;
+	double dblVal;
 } MockHW_t;
+ 
+enum SUIT_HW_PARAMS {
+	// RELAYS
+	E_RELAYS_FIRST,
+
+	E_LIGHTS = E_RELAYS_FIRST,
+	E_RELAYS_LIGHTS_AUTO,
+	E_LIGHTS_HEADWHITE,
+	E_HEADLIGHTS_RED,
+	E_HEAD_FANS,
+	E_WATER_PUMP,
+	E_ON_BUTTON,
+	E_PELTIER,
+	E_HIGH_CURRENT_LIVE,
+	E_HIGH_CURRENT_GROUND,
+
+	E_RELAYS_LAST = E_HIGH_CURRENT_GROUND,
+
+	// Temperatures
+	E_TEMP_FIST,
+
+	E_TEMP_HEAD = E_TEMP_FIST,
+	E_TEMP_ARMPIT,
+	E_TEMP_CROTCH,
+	E_TEMP_WATER,
+
+	E_TEMP_LAST = E_TEMP_WATER,
+
+	// Voltage
+	E_VOLTAGE_FIRST,
+
+	E_VOLTAGE_1 = E_VOLTAGE_FIRST,
+	E_VOLTAGE_2,
+
+	E_VOLTAGE_LAST = E_VOLTAGE_2,
+
+	// Flowrate
+	E_FLOWRATE,
+
+	// Count them:
+	E_NUM_HW_PARAMS
+};
+
+// Store the mock vales for HW.
+MockHW_t mock_data[E_NUM_HW_PARAMS];
+
+static bool is_initialized = false;
 
 // create socket and constantly read values from it and store values
 void halosuit_init() 
-{
+{.
+	is_initialized = true;
     //return NULL;
 }
 
@@ -27,31 +75,53 @@ void halosuit_exit()
 // 
 int halosuit_relay_switch(unsigned int relay, int ps)
 {
+	assert(relay <= HIGH_CURRENT_GROUND);
+
+	int mock_index = relay + E_RELAYS_FIRST;
+	
+	mock_data[mock_index].intVal = ps;
     return 0;
 }
 
 int halosuit_relay_value(unsigned int relay, int *value)
 {
-    return 0;
+	int mock_index = relay + E_RELAYS_FIRST;
+	value = mock_data[mock_index].intVal;
+
+	return 0;
 }
 
 int halosuit_temperature_value(unsigned int location, double *value)
 {
+	int mock_index = relay + E_RELAYS_FIRST;
+	value = mock_data[mock_index].dblVal;
+
     return 0;
 }
 
 int halosuit_flowrate(int *flow)
 {
+	if(!is_initialized) {
+		return -1;
+	}
+
+	flow = mock_data[E_FLOWRATE].intVal;
     return 0;
 }
 
 int halosuit_voltage_value(unsigned int battery, int *value)
 {
+	if(!is_initialized) {
+		return -1;
+	}
+	int mock_index = E_VOLTAGE_FIRST + battery - 1;
+	value = mock_data[mock_index].intVal;
     return 0;
 }
 
 int halosuit_current_draw_value(unsigned int battery, int *value)
 {
+	// requires calculations
     return 0;
 }
 
