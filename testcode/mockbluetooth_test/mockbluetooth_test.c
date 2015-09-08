b57b76f07d5930c3d7e1a71b3685681cfe8e3e3d
HamiltonChris <chris.ross.hamilton@gmail.com>
2015-09-08 15:50:43 -0700

    Organize testcode folder
    Rename test.c into beagleblue_test.c and move it into folder in
    testcode. Create mockbluetooth_test.c for testing the test module.

---
+++ testcode/mockbluetooth_test/mockbluetooth_test.c
@@ -0,0 +1,14 @@
+/*
+ * mockbluetooth_test.c
+ *
+ * This code is used to test the functionality of the mock_bluetooth.c
+ */
+
+
+#include <beagleblue/beagleblue.h>
+
+int main ()
+{
+
+    return 0;
+}
