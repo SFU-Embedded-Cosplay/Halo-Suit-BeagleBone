6988bd202f1684ef46da5aded183776a6ee5fe3f
Tyler-R <trubenui@sfu.ca>
2016-03-21 19:36:58 -0700

    Remove repreated code in logger_startup
    logger_startup was printing a message using the same format as logger_log and so I removed the duplicated code from logger_startup and replace it will a call to logger_log

--- halosuit/logger.c
+++ halosuit/logger.c
@@ -48,23 +48,13 @@ void logger_log(const char *message, ...)
 
 void logger_startup()
 {
-    time_t current_time = time(NULL);
-    char  time_string[1000];
-
-    struct tm * time = localtime(&current_time);
-    strftime(time_string, 1000, "%a, %d %b %Y %T %z", time);
-
-    if (current_time == ((time_t) - 1) || time_string == NULL) {
-        printf("Unable to compute time");
-        return;
-    }
-
-    FILE* f = fopen(LOG_FILE, "a");
-    if (f == NULL) {
+    FILE* log_file = fopen(LOG_FILE, "a");
+    if (log_file == NULL) {
         printf("Unable to open log file");
     } else {
-        fprintf(f, "\n###########################################################\n");
-        fprintf(f, "[%s] - %s\n", time_string, "Halosuit initializing");
-        fclose(f);
+        fprintf(log_file, "\n###########################################################\n");
+        fclose(log_file);
+
+        logger_log("Halosuit initializing");
     }
 }
