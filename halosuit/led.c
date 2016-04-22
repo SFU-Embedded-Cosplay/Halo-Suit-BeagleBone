a43ca867b10d020039ea475a3804d8c4d4e2d1de
Tyler-R <trubenui@sfu.ca>
2016-04-21 23:20:56 -0700

    Add initialization implementation to led module

--- halosuit/led.c
+++ halosuit/led.c
@@ -1,16 +1,33 @@
 #include <halosuit/led.h>
+#include <stdio.h>
+
+// to make a path do: path = LED_FILE_PATH + LED_USR_VALUE + ..._FILE_PATH_EXTENSION
+#define LED_FILE_PATH "/sys/class/leds/beaglebone:green:usr"
+#define TRIGGER_FILE_PATH_EXTENSION "/trigger"
+#define BRIGHTNESS_FILE_PATH_EXTENSION "/brightness"
+
+#define TRIGGER_MODE "none"
+
+#define ON_VALUE "1"
+#define OFF_VALUE "0"
 
 void led_init(led_t led) 
 {
-	
+	char file_path[80];
+	snprintf(file_path, sizeof(file_path), "%s%d%s", LED_FILE_PATH, led.usr, TRIGGER_FILE_PATH_EXTENSION);
+
+	FILE* led_trigger_file = fopen(file_path, "w");
+	fprintf(led_trigger_file, "%s", TRIGGER_MODE);
+	fclose(led_trigger_file);
+
 }
 
 void led_on(led_t led) 
 {
-	
+
 }
 
 void led_off(led_t led) 
 {
-	
+
 }
