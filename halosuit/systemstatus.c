c3c0479fdb1ae072762a63a1a5c111526a7dab18
Tyler-R <trubenui@sfu.ca>
2016-04-24 15:46:39 -0700

    Make setting system status turn on led's
    The led's are turned on in a binary fashion based
    on the value in the status.
    
    For example if the status value is 15 then all
    the leds will be turned on.  If the value in 3
    then the two right most led's will turn on.

--- halosuit/systemstatus.c
+++ halosuit/systemstatus.c
@@ -7,6 +7,33 @@ static led_t led1 = { 1 };
 static led_t led2 = { 2 };
 static led_t led3 = { 3 };
 
+static void update_led_display() 
+{
+	if(currentStatus % 2 == 1) {
+		led_on(led3);
+	} else {
+		led_off(led3);
+	}
+
+	if((currentStatus / 2) % 2 == 1) {
+		led_on(led2);
+	} else {
+		led_off(led2);
+	}
+
+	if((currentStatus / 4) % 2 == 1) {
+		led_on(led1);
+	} else {
+		led_off(led1);
+	}
+
+	if((currentStatus / 8) % 2 == 1) {
+		led_on(led0);
+	} else {
+		led_off(led0);
+	}
+}
+
 void systemstatus_init() {
 
 	led_init(led0);
@@ -18,7 +45,9 @@ void systemstatus_init() {
 }
 
 void systemstatus_set_status(status_t status) {
+	currentStatus = status;
 
+	update_led_display();
 }
 
 status_t systemstatus_get_status() {
