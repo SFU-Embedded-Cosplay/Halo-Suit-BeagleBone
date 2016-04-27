6c1d45c8c7663fc47277676a1c5137ecf64a1739
Tyler-R <trubenui@sfu.ca>
2016-04-27 13:44:50 -0700

    Fix code style issues

--- halosuit/systemstatus.c
+++ halosuit/systemstatus.c
@@ -1,7 +1,7 @@
 #include <halosuit/systemstatus.h>
 #include <halosuit/led.h>
 
-static status_t currentStatus = NO_STATUS;
+static status_t current_status = NO_STATUS;
 static led_t led0 = { 0 };
 static led_t led1 = { 1 };
 static led_t led2 = { 2 };
@@ -9,32 +9,33 @@ static led_t led3 = { 3 };
 
 static void update_led_display() 
 {
-	if(currentStatus % 2 == 1) {
+	if(current_status % 2 == 1) {
 		led_on(led3);
 	} else {
 		led_off(led3);
 	}
 
-	if((currentStatus / 2) % 2 == 1) {
+	if((current_status / 2) % 2 == 1) {
 		led_on(led2);
 	} else {
 		led_off(led2);
 	}
 
-	if((currentStatus / 4) % 2 == 1) {
+	if((current_status / 4) % 2 == 1) {
 		led_on(led1);
 	} else {
 		led_off(led1);
 	}
 
-	if((currentStatus / 8) % 2 == 1) {
+	if((current_status / 8) % 2 == 1) {
 		led_on(led0);
 	} else {
 		led_off(led0);
 	}
 }
 
-void systemstatus_init() {
+void systemstatus_init() 
+{
 
 	led_init(led0);
 	led_init(led1);
@@ -44,12 +45,14 @@ void systemstatus_init() {
 	systemstatus_set_status(BOOT_SUCCESS);
 }
 
-void systemstatus_set_status(status_t status) {
-	currentStatus = status;
+void systemstatus_set_status(status_t status) 
+{
+	current_status = status;
 
 	update_led_display();
 }
 
-status_t systemstatus_get_status() {
-	return currentStatus;
+status_t systemstatus_get_status() 
+{
+	return current_status;
 }
