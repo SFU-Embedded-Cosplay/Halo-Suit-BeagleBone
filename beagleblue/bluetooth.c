5ac30bf5c20bb24fa7f7cebadd8a77526fafe5ba
Tyler-R <trubenui@sfu.ca>
2016-04-30 11:54:28 -0700

    Make variable names more readable

--- beagleblue/bluetooth.c
+++ beagleblue/bluetooth.c
@@ -9,29 +9,29 @@
 
 void bluetooth_bind_socket(connection_t *connection) 
 {
-	struct sockaddr_rc loc_addr = { 0 };
+	struct sockaddr_rc local_address = { 0 };
 
 	if (connection->socket == -1) {
 		//initialize socket
 		connection->socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
 		//bind socket to local bluetooth device
-		loc_addr.rc_family = AF_BLUETOOTH;
-		loc_addr.rc_bdaddr = *BDADDR_ANY;
-		loc_addr.rc_channel = connection->channel;
-		bind(connection->socket, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+		local_address.rc_family = AF_BLUETOOTH;
+		local_address.rc_bdaddr = *BDADDR_ANY;
+		local_address.rc_channel = connection->channel;
+		bind(connection->socket, (struct sockaddr *)&local_address, sizeof(local_address));
 	}
 }
 
 void bluetooth_connect_client(connection_t *connection, char* buffer) 
 {
-	struct sockaddr_rc rem_addr = { 0 };
-	socklen_t opt = sizeof(rem_addr);
+	struct sockaddr_rc remote_address = { 0 };
+	socklen_t opt = sizeof(remote_address);
 
 	listen(connection->socket, 1);
 
-	connection->client = accept(connection->socket, (struct sockaddr *)&rem_addr, &opt);
-	ba2str( &rem_addr.rc_bdaddr, buffer);
+	connection->client = accept(connection->socket, (struct sockaddr *)&remote_address, &opt);
+	ba2str( &remote_address.rc_bdaddr, buffer);
 }
 
 // returns the same values as a call to send() would return
